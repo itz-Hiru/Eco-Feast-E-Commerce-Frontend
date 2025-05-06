7d120a80f36b7590a0bee0fa88a199d8b9f50ced
itz-Hiru <hirumithagithub@gmail.com>
2025-05-06 11:04:38 +0530

    Created Hero Banner

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroBanner from '../../components/Hero/HeroBanner'
+
+const Home = () => {
+  return (
+    <div className="mt-10">
+        <HeroBanner />
+    </div>
+  )
+}
+
+export default Home
