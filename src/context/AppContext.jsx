f6e2c48a2dce40ae2d1bcbe45d5d51c522246cf7
itz-Hiru <hirumithagithub@gmail.com>
2025-05-07 13:23:31 +0530

    Created top rated products section

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,16 +1,81 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+  const currency = import.meta.VITE_CURRENCY;
   const navigate = useNavigate();
 
   const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState({});
 
-  const value = { navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin };
+  // Fetch all products
+  const fetchProducts = async () => {
+    setProducts(dummyProducts);
+  };
+
+  // Add products to the cart
+  const addToCart = (itemId) => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+    setCartItems(cartData);
+    toast.success("Product added to cart.");
+  };
+
+  // Update cart items quantity
+  const updateCartItem = (itemId, quantity) => {
+    let cartData = structuredClone(cartItems);
+
+    cartData[itemId] = quantity;
+    setCartItems(cartData);
+    toast.success("Cart updated successfully.");
+  };
+
+  // Remove product item from cart
+  const removeCartItem = (itemId) => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      cartData[itemId] -= 1;
+      if (cartData[itemId] === 0) {
+        delete cartData[itemId];
+      }
+    }
+
+    toast.success("Product removed from cart.");
+    setCartItems(cartData);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+    products,
+    currency,
+    addToCart,
+    updateCartItem,
+    removeCartItem,
+    cartItems,
+  };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import HeroBanner from '../components/Home/HeroBanner.component'
-import Categories from '../components/Home/Categories.component'
+import HeroBanner from '../sections/Home/HeroBanner.section'
+import Categories from '../sections/Home/Categories.section'
+import TopFood from '../sections/Home/TopFood.section'
 
 const Home = () => {
   return (
     <div className="mt-10">
         <HeroBanner />
         <Categories />
+        <TopFood />
     </div>
   )
 }
